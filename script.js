3ab3ef55138cf1b780324b5944ab92acbd87a416
Dobrunia <dobkos@hotmail.com>
2023-03-10 23:25:21 +0300

    7

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ var a = 9;
 //Declare String Variables
 var myFirstName = "Dobrunia";
 var myLastName = "Kost";
+
+//Understanding Uninitialized Variables
+var a;
+var b;
+var c;
+a = 5;
+b = 10;
+c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//Understanding Case Sensitivity in Variables
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Explore Differences Between the var and let Keywords
+let catName = "Oliver";
+let catSound = "Meow!";
