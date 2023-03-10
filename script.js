467b9d415b0aa2e041477d2402e0d5c8daf5b8ff
Dobrunia <dobkos@hotmail.com>
2023-03-10 23:19:31 +0300

    5

--- script.js
+++ script.js
@@ -11,4 +11,15 @@ var myName;
 var a;
 a = 7;
 
-//
+//Assigning the Value of One Variable to Another
+var a;
+a = 7;
+var b;
+b = a;
+
+//Initializing Variables with the Assignment Operator
+var a = 9;
+
+//Declare String Variables
+var myFirstName = "Dobrunia";
+var myLastName = "Kost";
