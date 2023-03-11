3aade9f17c00b956c6166ef744542039ef53d6a9
Dobrunia <dobkos@hotmail.com>
2023-03-11 21:05:27 +0300

    fgbf

--- script.js
+++ script.js
@@ -66,3 +66,79 @@ const product = 8 * 10;
 
 //Divide One Number by Another with JavaScript
 const quotient = 66 / 33;
+
+//Increment a Number with JavaScript
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+//Decrement a Number with JavaScript
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+//Create Decimal Numbers with JavaScript
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal = 5.7;
+
+//Multiply Two Decimals with JavaScript
+const product = 2.0 * 2.5;
+
+//Divide One Decimal by Another with JavaScript
+const quotient = 4.4 / 2.0; // Change this line
+
+//Finding a Remainder in JavaScript
+const remainder = 11 % 3;
+
+//Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//Compound Assignment With Augmented Subtraction
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+//Compound Assignment With Augmented Multiplication
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+//Compound Assignment With Augmented Division
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+//Quoting Strings with Single Quotes
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//Escape Sequences in Strings
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
