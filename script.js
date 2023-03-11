773ef34d10c10a078940d89d0d2fdb271c193e0a
Dobrunia <dobkos@hotmail.com>
2023-03-11 21:08:58 +0300

    f

--- script.js
+++ script.js
@@ -142,3 +142,11 @@ const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 
 //Escape Sequences in Strings
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//Concatenating Strings with Plus Operator
+const myStr = "This is the start. " + "This is the end."; // Change this line
+
+//Concatenating Strings with the Plus Equals Operator
+let myStr;
+myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
